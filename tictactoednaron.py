711deb8f31b426ca37552c4c97ce8cc7d627cafd
tria <tria@email.email.com>
2026-09-04 21:15:23 +0700

    finished winning condition

--- tictactoednaron.py
+++ tictactoednaron.py
@@ -6,3 +6,15 @@ def show_board():
     for i in range (0,9,3):
         print (board[i:i+3])
 show_board()
+
+def winning_condition (player):
+    return (
+    (board [0] == player and board [1]== player and board [2] == player) or
+    (board [3] == player and board [4]== player and board [5] == player) or
+    (board [6] == player and board [7] == player and board [8] == player) or
+    (board [0] == player and board [3] == player and board [6] == player ) or
+    (board [1] == player and board [4] == player and board [7] == player) or
+    (board [2] == player and board [5] == player and board [8] == player) or
+    (board [0] == player and board [5] == player and board [8]== player) or
+    (board [2] == player and board [5] == player and board [6] == player))
+    
